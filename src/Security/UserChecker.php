43c78722cacb0995b6f44ee178a53e1898175953
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-06-10 14:48:24 +0200

    Add a user checker to verify if user account is enable

---
+++ src/Security/UserChecker.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+
+class UserChecker implements UserCheckerInterface
+{
+    public function checkPreAuth(UserInterface $user): void
+    {
+        if ($user instanceof User && !$user->isActive()) {
+            throw new CustomUserMessageAccountStatusException('Votre compte est suspendu. Contactez un administrateur.');
+        }
+    }
+
+    public function checkPostAuth(UserInterface $user): void
+    {
+    }
+}
