abe40e28b87ff8f1bad2f33c8a808e9e621b48b6
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-06-10 09:53:49 +0200

    Add restriction for edit, add, update album paths

--- src/Controller/Admin/AlbumController.php
+++ src/Controller/Admin/AlbumController.php
@@ -13,6 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class AlbumController extends AbstractController
 {
@@ -26,6 +27,7 @@ class AlbumController extends AbstractController
 
     }
 
+    #[IsGranted("ROLE_ADMIN")]
     #[Route('/admin/album', name: 'admin_album_index')]
     public function index() : Response
     {
@@ -34,7 +36,7 @@ class AlbumController extends AbstractController
         return $this->render('admin/album/index.html.twig', ['albums' => $albums]);
     }
 
-
+    #[IsGranted("ROLE_ADMIN")]
     #[Route('/admin/album/add', name: 'admin_album_add')]
     public function add(Request $request) : Response
     {
@@ -52,6 +54,7 @@ class AlbumController extends AbstractController
         return $this->render('admin/album/add.html.twig', ['form' => $form->createView()]);
     }
 
+    #[IsGranted("ROLE_ADMIN")]
     #[Route('/admin/album/update/{id}', name: 'admin_album_update')]
     public function update(Request $request, #[MapEntity(id:'id')] Album $album): Response
     {
@@ -67,6 +70,7 @@ class AlbumController extends AbstractController
         return $this->render('admin/album/update.html.twig', ['form' => $form->createView()]);
     }
 
+    #[IsGranted("ROLE_ADMIN")]
     #[Route('/admin/album/delete/{id}', name: 'admin_album_delete')]
     public function delete(#[MapEntity(id:'id')] Album $album): Response
     {
