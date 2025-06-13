63ebcf85010a9ce8674d51ff2edc648348f6f688
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-06-13 15:31:44 +0200

    Add Login Test

---
+++ tests/Functional/Security/LoginTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Tests\Functional\Security;
+
+use App\Entity\User;
+use App\Tests\Functional\FunctionalTestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\PasswordHasher\PasswordHasherInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+class LoginTest extends FunctionalTestCase
+{
+    public function testThatLoginShouldSucceeded(): void
+    {
+        $this->get('/login');
+
+        $this->client->submitForm('Connexion', [
+            '_username' => 'invite+1@exemple.com',
+            '_password' => 'password',
+        ]);
+        $this->assertResponseStatusCodeSame(302);
+        $this->client->followRedirect();
+
+
+        $this->assertStringNotContainsString('Connexion', $this->client->getResponse()->getContent());
+
+//        $this->get('/logout');
+//        self::assertFalse($authorizationChecker->isGranted('IS_AUTHENTICATED'));
+    }
+
+    public function testThatLoginShouldFailed(): void
+    {
+        $this->get('/login');
+
+        $this->client->submitForm('Connexion', [
+            '_username' => 'invite+1@exemple.com',
+            '_password' => 'fail'
+        ]);
+
+        $this->client->followRedirect();
+
+        $this->assertSelectorTextContains('.alert-danger', 'Invalid credentials.');
+    }
+
+
+
+}
