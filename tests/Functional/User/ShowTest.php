8f586b92135e1ea0b04e826ba7bf900c2b0df860
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-06-16 15:54:19 +0200

    Add test show user page

---
+++ tests/Functional/User/ShowTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Tests\Functional\User;
+
+use App\Tests\Functional\FunctionalTestCase;
+
+class ShowTest extends FunctionalTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->login();
+    }
+
+    public function testAdminShowGuestsPage(): void
+    {
+        $this->login("ina@zaoui.com");
+        $this->get("/admin/guests");
+        self::assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('main h1', 'Invités');
+
+    }
+
+    public function testUserShowGuestsPage(): void
+    {
+        $this->get("/admin/guests");
+        $this->assertResponseStatusCodeSame(403);
+
+    }
+
+    public function testGuestShowGuestsPage(): void
+    {
+        $this->get("/logout");
+        $this->get("/admin/guests");
+        self::assertResponseRedirects('/login');
+    }
+}
